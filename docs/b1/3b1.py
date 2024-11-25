843f7ef4bcd569441856740a7d7a53dc301dcf48
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 07:12:25 -0600

    Create 3b1.py

---
+++ docs/b1/3b1.py
@@ -0,0 +1,11 @@
+# programa 3: imprime tres tipos de datos
+# Fecha de elaboracion: 2024/10/14
+# Elaborado por: Edgar Alejandro Esparza Reyes
+# Imprimir el nummero de dias en una semana
+print (7) #numero entero
+
+# Imprimir el nombre de una figura de 3 lados
+print ("Triangulo") #alfanumerico o string
+
+#imprimir a cuantos metros equivalen 250 cm 
+print (0.25) #tipo de dato float
