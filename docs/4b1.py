0c8b9915a4b9fdcb75a12642bd32aa3a494a764b
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 07:18:00 -0600

    Create 4b1.py

---
+++ docs/4b1.py
@@ -0,0 +1,9 @@
+# programa 4: comprobar el codigo
+# Fecha de elaboracion: 2024/10/14
+# Elaborado por: Edgar Alejandro Esparza Reyes
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+
+print("sumando cadenas " + "esta bien")
+#print("sumando cadenas " - "esta bien")
