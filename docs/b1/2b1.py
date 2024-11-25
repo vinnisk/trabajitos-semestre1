45f3dff4ed0f16ce95643da335ba98a733fcfe10
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 07:05:32 -0600

    Create 2b1.py

---
+++ docs/b1/2b1.py
@@ -0,0 +1,5 @@
+# programa 2: que solicite el nombre al usuario y lo imprime con un mensaje
+# Fecha de elaboracion: 2024/10/14
+# Elaborado por: Edgar Alejandro Esparza Reyes
+nom = input("Digite su nombre: ")
+print("Hola, "+nom) 
