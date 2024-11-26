8a7e255a15bc1da351fd7656937ecf5917ed4efb
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 16:17:51 -0600

    Create 6b3.py

---
+++ docs/b3/6b3.py
@@ -0,0 +1,15 @@
+# programa 6: Listas
+# Fecha de elaboracion: 2024/10/30
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+# Lista de enteros
+print([20, 10, 35])
+
+# Lista de cadenas de texto
+print (["manzana", "piña", "naranja"])
+
+# Lista de booleanos
+print([False, True, True])
+
+# Lista vacia
+print([])
