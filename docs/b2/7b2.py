842bf5c73c6eb62172b7a0a3e37b2ffb03f2fabe
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 09:11:25 -0600

    Create 7b2.py

---
+++ docs/b2/7b2.py
@@ -0,0 +1,12 @@
+# programa 7: bar
+# Fecha de elaboracion: 2024/10/24
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+print("Nesecita ser mayor a 18 años para pasar al bar")
+
+edad = int(input("ingrese su edad:"))
+
+if (edad>=18):
+    print("ingrese a enbriagarse")
+else:
+    print("no entras")
