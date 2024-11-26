4154037957b92c8531a5edb1b6747bef34a8ba3d
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 20:09:49 -0600

    Create 5b1.py

---
+++ docs/b1/5b1.py
@@ -0,0 +1,13 @@
+# programa 5: solicitar dos numeros para realizar operaciones aritmeticas
+# Fecha de elaboracion: 2024/10/14
+# Elaborado por: Edgar Alejandro Esparza Reyes
+num1 = float(input("Digite el primer número: "))
+num2 = float(input("Digite el segundo número: "))
+suma = num1 + num2
+resta = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+print("El valor de la suma: " + str(suma))
+print("El valor de la resta: " + str(resta))
+print("El valor de la multiplicación: " + str(mult))
+print("El valor de la división: " + str(div))
