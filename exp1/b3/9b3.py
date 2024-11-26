af57a3aca887b79042109f3f64fa67ae5a905055
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 16:44:21 -0600

    Create 9b3.py

---
+++ exp1/b3/9b3.py
@@ -0,0 +1,20 @@
+# programa 9: IGUALDAD DE LISTAS 
+# Fecha de elaboracion: 2024/10/31
+# Elaborado por: Edgar Alejandro 
+
+puntos = ["10", "30", "20"]
+puntos2 = [10, 30, 20]
+ordenados = [10, 20, 30]
+puntos_texto = ["10", "30", "20",]
+
+print(puntos == puntos2)
+
+print(puntos == ordenados)
+
+print(puntos == puntos_texto)
+
+print(puntos != puntos)
+
+print(puntos != ordenados)
+
+print(puntos != puntos_texto)
