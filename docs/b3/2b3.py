a9ffd51c4e1f4f6551073da90c7d8fe7170d2700
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 14:48:25 -0600

    Create 2b3.py

---
+++ docs/b3/2b3.py
@@ -0,0 +1,13 @@
+# programa 2: else + if 2
+# Fecha de elaboracion: 2024/10/28
+# Elaborado por: Edgar Alejandro 
+
+num1 = int(input("ingrese numero uno:"))
+num2 = int(input("ingrese numero dos:"))
+
+if num1 < num2:
+    print("el numero 1 es menor que el numero dos")
+elif num1 ==  num2:
+    print("ambos numeros son iguaes")
+else:
+    print("el numero 1 es mayor al numero 2")
