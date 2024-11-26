569642472bb0e024815dc65ec3b77c2762f46b10
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 15:03:49 -0600

    Create 3b3.py

---
+++ docs/b3/3b3.py
@@ -0,0 +1,12 @@
+# programa 3: identificacion de tipo de datos
+# Fecha de elaboracion: 2024/10/28
+# Elaborado por: Edgar Alejandro
+
+variable = input("ingrese su edad")
+print(type(variable))
+
+variable = int(variable)
+print(type(variable))
+
+variable = int(variable)
+print(type(variable))
