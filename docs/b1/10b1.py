4c4684695b93ea4ae01972b45bd7184333350111
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 22:24:14 -0600

    Create 10b1.py

---
+++ docs/b1/10b1.py
@@ -0,0 +1,9 @@
+# programa 10: unicode
+# Fecha de elaboracion: 2024/10/18
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+print("eo \u263A")
+print("eo \u213A")
+print("eo \u223A")
+print("eo \u253A")
+print("eo \u243A")
