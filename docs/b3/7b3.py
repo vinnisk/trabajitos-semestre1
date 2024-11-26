121af5aa4189bfc12757d2a710b470b3ef9c661a
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 16:26:51 -0600

    Create 7b3.py

---
+++ docs/b3/7b3.py
@@ -0,0 +1,27 @@
+# Programa 7: Tipos de dato, listas
+# Fecha de elaboracion: 2024/10/30
+# Elaborado por: Edgar Alejandro
+
+# Cadenas
+cadena = ["pedro", "Juan", "chorchis"]
+
+print(cadena)
+print(cadena[1])
+print(type(cadena))
+print(type(cadena[1]))
+
+# int
+cadena = [81, 55, 9]
+
+print(cadena)
+print(cadena[1])
+print(type(cadena))
+print(type(cadena[1]))
+
+# booleano
+cadena = [True, False, False]
+
+print(cadena)
+print(cadena[1])
+print(type(cadena))
+print(type(cadena[1]))
