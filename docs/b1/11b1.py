e31d9f3003e201f687b5758a52e4b30abc95fc6c
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 22:33:36 -0600

    Create 11b1.py

---
+++ docs/b1/11b1.py
@@ -0,0 +1,33 @@
+# programa 11: analizar los operadores de comparacion
+# Fecha de elaboracion: 2024/10/18
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+print("operador ==")
+print(5 == 7) #respuesta false
+print(5 == 5) #respuesta true
+print(5.0 == 5, "\n") #respuesta true
+
+print("operador !=")
+print(5 != 7) #respuesta true
+print(5 != 5) #respuesta false
+print(5.0 != 5, "\n") #respuesta false
+
+print("operador >")
+print(5 > 7) #respuesta false
+print(5 > 5) #respuesta false
+print(5.0 > 5, "\n") #respuesta false
+
+print("operador <")
+print(5 < 7) #respuesta true
+print(5 < 5) #respuesta false
+print(5.0 < 5, "\n") #respuesta false
+
+print("operador >=")
+print(5 >= 7) #respuesta false
+print(5 >= 5) #respuesta true
+print(5.0 >= 5, "\n") #respuesta true
+
+print("operador <=")
+print(5 <= 7) #respuesta true
+print(5 <= 5) #respuesta true
+print(5.0 <= 5) #respuesta true
