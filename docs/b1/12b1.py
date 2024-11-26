25a994c0dfa71c2f94dab6e958fd06a8f85e4d62
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 07:21:30 -0600

    Create 12b1.py

---
+++ docs/b1/12b1.py
@@ -0,0 +1,15 @@
+#Programa 1.12 P/Probar las compuertas lógicas AND, OR y NOT
+# Elaborado por: Edgar Alejandro Esparza Reyes
+# Fecha: 2024/10/18
+
+print("Compuerta OR")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print("\n compuerta AND")
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
