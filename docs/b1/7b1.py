15f56f7e2ad5db8510f565d0a672ee1c7e09971a
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 21:40:58 -0600

    Create 7b1.py

---
+++ docs/b1/7b1.py
@@ -0,0 +1,11 @@
+# programa 7: area y perimetro de un circulo
+# Fecha de elaboracion: 2024/10/16
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+rad = float(input("digite el radio:"))
+
+area = 3.1416 * rad ** 2
+per = 2 * 3.1416 * rad
+
+print("El area del circulo es: " + str(area))
+print("El perimetro del circulo es: " + str(per))
