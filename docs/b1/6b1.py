4abc96b12b81d3f413ee01ca731b590b12e19ed8
vinnisk <edgar.esparzza27@gmail.com>
2024-11-25 21:12:27 -0600

    Create 6b1.py

---
+++ docs/b1/6b1.py
@@ -0,0 +1,8 @@
+# programa 6: Calcular el area de un triangulo
+# Fecha de elaboracion: 2024/10/15
+# Elaborado por: Edgar Alejandro Esparza Reyes
+num1 = float(input("Digite la base del triangulo: "))
+num2 = float(input("Digite la altura del triangulo: "))
+res = num1 * num2 / 2
+
+print("El area del triangulo es: " + str(res))
