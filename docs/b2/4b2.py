ca7302abe5aa944977a6eb3119fde649e842cb07
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 11:04:13 -0600

    Create 4b2.py

---
+++ docs/b2/4b2.py
@@ -0,0 +1,5 @@
+# Programa 4: Programa que calcula los impuestos de un valor e imprime.
+# Fecha: 2024/10/22
+# Realizado por: Edgar Alejandro Esparza Reyes
+valor = float(input("Digite el valor para calcular el iva: "))
+print("El impuesto es: ", valor * 0.16)
