7dab844e033205c186c648393aea6649023ae5d0
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 08:25:49 -0600

    Create 6b2.py

---
+++ docs/b2/6b2.py
@@ -0,0 +1,11 @@
+# programa 6: analizar los operadores de comparacion
+# Fecha de elaboracion: 2024/10/24
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+#Negative subscript Indices
+word = "hamster"
+
+print(word[-1])
+print(word[1:-1])
+print(word[-3:])
+print(word[:3])
