2697468fbd4025a58bbce485b5aca641381664b8
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 14:21:32 -0600

    Create 1b3.py

---
+++ docs/b3/1b3.py
@@ -0,0 +1,13 @@
+# programa 1: else + if
+# Fecha de elaboracion: 2024/10/28
+# Elaborado por: Edgar Alejandro Esparza Reyes
+
+nombre = "hamster"
+
+if "perro" in nombre:
+    print("Es un perro")
+elif "gato" in nombre:
+    print("Es un gato")
+else:
+    print("Mascota desconocida.")
+print("gracias por usar el programa")
