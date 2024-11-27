d5fedc7accc50421e9187518ee75128d9496a5ab
vinnisk <edgar.esparzza27@gmail.com>
2024-11-26 18:36:14 -0600

    Create 10b3.py

---
+++ docs/b3/10b3.py
@@ -0,0 +1,9 @@
+# programa 10: Operador in and in not
+# Fecha de elaboracion: 2024/10/31
+# Elaborado por: Edgar Alejandro
+
+nombres = ["choto", "emilio", "Luis"]
+print ("Luis" in nombres)
+print ("emi" in nombres)
+print ("javier" in nombres)
+print ("jose" not in nombres)
