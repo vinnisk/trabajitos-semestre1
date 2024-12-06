5d6336bc305d853350287b70a3d69001ca78586a
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 16:42:21 -0600

    Create 4b4.py

---
+++ exp1/b4/4b4.py
@@ -0,0 +1,8 @@
+# programa 4: Nombres de personajes
+# Fecha de elaboracion: 2024/11/4
+# Elaborado por: Edgar Alejandro Reyes
+x = ["baam", "khun", "shibisu", "Zahard", "V", "arlene", "androssi", "rak" "ran" "edhan"]
+print(x)
+
+for i in x:
+    print("el nombre es", i)
