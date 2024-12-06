1eac9493ff920042f2e57fbb2b898f5619ef92a9
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 15:14:33 -0600

    Create 3b4.py

---
+++ docs/b4/3b4.py
@@ -0,0 +1,20 @@
+# programa 3: Funcion range
+# Fecha de elaboracion: 2024/11/4
+# Elaborado por: Edgar Alejandro Esparza Reyes
+x = range(10)
+print(x)
+
+print("Imprime los valores del 0 al 15")
+for num in x:
+    print(num)
+
+print("imprime los valores del 5 al 15")
+x1 = range(5,16)
+
+for num in x1:
+    print(num)
+
+print("\nImprime los pares del 10 al 20")
+x2 = range(10, 21, 2)
+for num in x2:
+    print(num)
