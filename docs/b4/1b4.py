b57e30098912d3babb4fa9ff665f55681384aa38
vinnisk <edgar.esparzza27@gmail.com>
2024-12-05 21:51:08 -0600

    Create 1b4.py

---
+++ docs/b4/1b4.py
@@ -0,0 +1,13 @@
+# programa 1: Ciclo for
+# Fecha de elaboracion: 2024/11/4
+# Elaborado por: Edgar Alejandro Esparza Reyes
+lista = [10, 30, 40, 20, 35, 80]
+
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+print(lista[5])
+
+for i in lista:
+    print(i)
