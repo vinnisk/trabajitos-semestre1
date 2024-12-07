2d5fec5a4a63908ec704c9a034392ad3247177e3
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 18:48:46 -0600

    Create 10b4.py

---
+++ docs/b4/10b4.py
@@ -0,0 +1,10 @@
+# programa 10: Programa para demostrar los metodos lower() y upper()
+# Fecha de elaboracion: 2024/11/8
+# Elaborado por: Edgar Alejandro Reyes
+
+cadena = "Python es un lenguaje de programacion"
+print("\n", cadena)
+cadenam = cadena.upper()
+print("\n", cadenam)
+cadenami = cadena.lower()
+print("\n", cadenami)
