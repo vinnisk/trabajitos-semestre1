fc2e52f91125e90aabf542fd0451a99d60298c70
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 18:35:39 -0600

    Create 8b4.py

---
+++ docs/b4/8b4.py
@@ -0,0 +1,9 @@
+# programa 8: ciclo while
+# Fecha de elaboracion: 2024/11/7
+# Elaborado por: Edgar Alejandro Esparza
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+print("final del programa")
