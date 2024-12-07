a558abe0a3eb69691a3a2cb14026ded7fb7323ce
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 19:29:28 -0600

    Create 4b5.py

---
+++ docs/b5/4b5.py
@@ -0,0 +1,12 @@
+# Programa 4: Ejemplo de funcion
+# Fecha: 13/11/2024
+# Realizado por: Edgar Alejandro
+
+#Definicion de la funcion
+def saludar (nombre):
+    print(f"¡Hola, {nombre}!")
+
+#Llamar la Función
+saludar("Edgar")
+saludar("PEPE")
+saludar("Choto")
