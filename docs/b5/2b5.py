9228928d09877cb274171b7a095354edfb1c8a56
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 19:55:48 -0600

    Create 2b5.py

---
+++ docs/b5/2b5.py
@@ -0,0 +1,16 @@
+# programa 2: Funciones con numeros
+# Fecha de elaboracion: 2024/11/13
+# Elaborado por: Edgar Alejandro
+
+print(abs(-10))  # Output: 10
+
+print(int("20"))  # Output: 20
+
+print(float("3.14"))  # Output: 3.14    
+
+print(bool(0))  # Output: False
+
+print(round(3.1416))
+
+num = [1, 2, 3, 4, 5]
+print(sum(num))  # Output: 15
