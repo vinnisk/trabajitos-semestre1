dea65e6a8ada8b45c5390def6d61a030d488ddf6
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 19:02:06 -0600

    Create 12b4.py

---
+++ docs/b4/12b4.py
@@ -0,0 +1,7 @@
+# programa 12: print(f"")
+# Fecha de elaboracion: 2024/12/8
+# Elaborado por: Edgar Alejandro
+nombre = "Edinguer"
+edad = 27
+cal = 93.8
+print (f"Nombre: {nombre}. edad: {edad}, Calificacion: {cal}")
