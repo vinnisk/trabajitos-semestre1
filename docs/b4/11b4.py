4e88592c52e885e742c51a160946680d4d11c922
vinnisk <edgar.esparzza27@gmail.com>
2024-12-06 18:53:24 -0600

    Create 11b4.py

---
+++ docs/b4/11b4.py
@@ -0,0 +1,10 @@
+# programa 11: condicinales
+# Fecha de elaboracion: 2024/11/8
+# Elaborado por: Edgar Alejandro Esparza
+
+palabra = ""
+while palabra != "salir":
+    palabra = input("ingrese la palabra \"salir\" para terminar el programa \n")
+    print("ingreso: ", palabra)
+    palabra = palabra.lower()
+print("fin del programa")
